c5bf06dbd997c3b8285b51387efea0cda23f1841
PARTNERS\pchangul <pchangul@cisco.com>
2021-08-10 12:35:59 +0530

    removed unwanted code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
 import { Route, Switch } from "react-router";
-import AddRecipe from './components/add-recipe/AddRecipe';
-import Navbar from './components/navbar/Navbar';
-import SignIn from './components/signin/signin';
-import RecipeList from './components/recipe-list/RecipeList';
-import RecipeDetails from './components/recipe-details/RecipeDetails';
-import SignUp from './components/signup/Signup';
-import { Redirect } from 'react-router-dom';
+import { Redirect } from "react-router-dom";
+import AddRecipe from "./components/add-recipe/AddRecipe";
+import RecipeDetails from "./components/recipe-details/RecipeDetails";
+import RecipeList from "./components/recipe-list/RecipeList";
+import SignIn from "./components/signin/signin";
+import SignUp from "./components/signup/Signup";
 
 function App() {
   return (
     <div className="container">
-      {/* <Navbar /> */}
       <Switch>
         <Route path="/signup" component={SignUp} />
         <Route path="/signin" component={SignIn} />
         <Route path="/recipe-list" component={RecipeList} />
         <Route path="/add-recipe" component={AddRecipe} />
         <Route path="/recipe-details/:id" component={RecipeDetails} />
-        <Redirect  from="/" to="/signup" />
+        <Redirect from="/" to="/signup" />
       </Switch>
     </div>
   );
