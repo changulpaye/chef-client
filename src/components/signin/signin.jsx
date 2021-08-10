0962ad91d884d2e49032534dfdaad6c59b702437
PARTNERS\pchangul <pchangul@cisco.com>
2021-08-10 12:38:18 +0530

    input type changed to password

--- src/components/signin/signin.jsx
+++ src/components/signin/signin.jsx
@@ -4,17 +4,23 @@ import logo from "../../assets/images/logo.jpg";
 import { useHistory } from "react-router-dom";
 
 const SignIn = () => {
+  
   const history = useHistory();
 
+  /** Navigation upon successful login */
   const handleSignIn = () => {
-    history.replace('/recipe-list');
-  }
-  
+    history.replace("/recipe-list");
+  };
+
   return (
     <div className="signup-container">
       <img src={logo} className="logo-container" alt="logo" />
       <input className="user-input med-top-mar" placeholder="Username" />
-      <input className="user-input med-top-mar" placeholder="Password" />
+      <input
+        type="password"
+        className="user-input med-top-mar"
+        placeholder="Password"
+      />
       <button className="button" onClick={handleSignIn}>
         Sign In
       </button>
